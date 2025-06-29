b2d4ebdf1e15d754b73994e6b3ed815e9e341243
Hendrik <li.zhiqiang2011@gmail.com>
2025-06-29 22:45:19 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+
+  return (
+    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
